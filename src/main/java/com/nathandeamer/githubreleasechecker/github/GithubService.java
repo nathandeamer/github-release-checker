8d8ab7710857f7674a2a49c5350e4d1e8a455a59
nathandeamer <nd@nathandeamer.com>
2022-11-15 15:15:21 +0000

    removed unused import

--- src/main/java/com/nathandeamer/githubreleasechecker/github/GithubService.java
+++ src/main/java/com/nathandeamer/githubreleasechecker/github/GithubService.java
@@ -1,6 +1,5 @@
 package com.nathandeamer.githubreleasechecker.github;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.util.Strings;
 import org.kohsuke.github.GHCompare;
